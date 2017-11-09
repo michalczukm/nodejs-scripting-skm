b203c7127306cd9c0fbccc8b6a8b212591b186e0
Michal Michalczuk <michalczukm@gmail.com>
2017-11-10 00:24:29 +0100

    move to typescript

--- gulp-example/src/index.js
+++ gulp-example/src/index.js
@@ -1,98 +1,77 @@
-(() => {
-  const $usersList = $('#users-list');
-  let users = [
-    { id: 1, username: 'Jan' },
-    { id: 2, username: 'Piotr' },
-    { id: 3, username: 'Adam' },
-    { id: 4, username: 'Maria' },
-    { id: 5, username: 'Kasia'}
-  ];
-
-  const getUsers = () => {
-    $usersList.empty();
-    users.map(u => UsersBuilder.$buildUser(u).appendTo($usersList));
-  }
-
-  const addUser = (event) => {
-    event.preventDefault();
-
-    let $newItem = $('#username');
-    let username = $newItem.val();
-
-    if (!username) {
-      return;
-    }
-
-    $newItem.val('');
-
-    users.push({
-      id: Math.max(...users.map(u => u.id), 0) + 1,
-      username: username
+"use strict";
+(function () {
+    var $usersList = $('#users-list');
+    var users = [
+        { id: 1, username: 'Jan' },
+        { id: 2, username: 'Piotr' },
+        { id: 3, username: 'Adam' },
+        { id: 4, username: 'Maria' },
+        { id: 5, username: 'Kasia' }
+    ];
+    var getUsers = function () {
+        $usersList.empty();
+        users.map(function (u) { return UsersBuilder.$buildUser(u).appendTo($usersList); });
+    };
+    var addUser = function (event) {
+        event.preventDefault();
+        var $newItem = $('#username');
+        var username = $newItem.val();
+        if (!username) {
+            return;
+        }
+        $newItem.val('');
+        users.push({
+            id: Math.max.apply(Math, users.map(function (u) { return u.id; }).concat([0])) + 1,
+            username: username
+        });
+        toastr.success('Item created', 'Success!');
+        getUsers();
+    };
+    var deleteUser = function (event) {
+        event.preventDefault();
+        var id = $(event.target.parentElement).data('id');
+        users = users.filter(function (u) { return u.id != id; });
+        toastr.success('Item deleted', 'Success!');
+        getUsers();
+    };
+    var updateUser = function (event) {
+        event.preventDefault();
+        var $userRow = $(event.target.parentElement);
+        var id = $userRow.data('id');
+        var user = users.find(function (u) { return u.id == id; });
+        if (!user) {
+            return;
+        }
+        var useItOnSuccess = function () { return $userRow.find('.edit-user-button, .delete-user-button').show(); };
+        var username = $userRow.find('.username').val();
+        user.username = username;
+        toastr.success('Item updated', 'Success!');
+        getUsers();
+    };
+    var editUser = function (event) {
+        event.preventDefault();
+        var $userRow = $(event.target.parentElement);
+        var $userName = $userRow.find('.username');
+        $userRow.find('.username').prop('disabled', false);
+        $userRow.find('.edit-user-button, .delete-user-button').hide();
+        $userRow.find('.edit-cancel-user-button, .update-user-button').show();
+        $userRow.data('edited-value', $userName.val());
+    };
+    var cancelUserEdit = function (event) {
+        event.preventDefault();
+        var $userRow = $(event.target.parentElement);
+        var $userName = $userRow.find('.username');
+        $userRow.find('.username').prop('disabled', true);
+        $userRow.find('.edit-user-button, .delete-user-button').show();
+        $userRow.find('.edit-cancel-user-button, .update-user-button').hide();
+        $userName.val($userRow.data('edited-value'));
+    };
+    $(function () {
+        getUsers();
+        $('#add-user-button').click(function (event) { return addUser(event); });
+        $('body').on('click', '.delete-user-button', function (event) { return deleteUser(event); });
+        $('body').on('click', '.edit-user-button', function (event) { return editUser(event); });
+        $('body').on('click', '.edit-cancel-user-button', function (event) { return cancelUserEdit(event); });
+        $('body').on('click', '.update-user-button', function (event) { return updateUser(event); });
     });
-
-    toastr.success('Item created', 'Success!');
-    getUsers();
-  }
-
-  const deleteUser = (event) => {
-    event.preventDefault();
-
-    const id = $(event.target.parentElement).data('id');
-
-    users = users.filter(u => u.id != id);
-
-    toastr.success('Item deleted', 'Success!');
-    getUsers();
-  }
-
-  const updateUser = (event) => {
-    event.preventDefault();
-    const $userRow = $(event.target.parentElement);
-
-    const useItOnSuccess = () => $userRow.find('.edit-user-button, .delete-user-button').show();
-
-    const id = $userRow.data('id');
-    const username = $userRow.find('.username').val();
-
-    users.find(u => u.id == id).username = username;
-
-    toastr.success('Item updated', 'Success!');
-    getUsers();
-  }
-
-  const editUser = (event) => {
-    event.preventDefault()
-
-    const $userRow = $(event.target.parentElement);
-    const $userName = $userRow.find('.username');
-
-    $userRow.find('.username').prop('disabled', false);
-    $userRow.find('.edit-user-button, .delete-user-button').hide();
-    $userRow.find('.edit-cancel-user-button, .update-user-button').show();
-
-    $userRow.data('edited-value', $userName.val());
-  }
-
-  const cancelUserEdit = (event) => {
-    event.preventDefault()
-
-    const $userRow = $(event.target.parentElement);
-    const $userName = $userRow.find('.username');
-
-    $userRow.find('.username').prop('disabled', true);
-    $userRow.find('.edit-user-button, .delete-user-button').show();
-    $userRow.find('.edit-cancel-user-button, .update-user-button').hide();
-
-    $userName.val($userRow.data('edited-value'));
-  }
-
-  $(() => {
-    getUsers();
-
-    $('#add-user-button').click((event) => addUser(event));
-    $('body').on('click', '.delete-user-button', (event) => deleteUser(event));
-    $('body').on('click', '.edit-user-button', (event) => editUser(event));
-    $('body').on('click', '.edit-cancel-user-button', (event) => cancelUserEdit(event));
-    $('body').on('click', '.update-user-button', (event) => updateUser(event));
-  })
 })();
---
+++ gulp-example/src/index.ts
@@ -0,0 +1,101 @@
+(() => {
+  const $usersList = $('#users-list');
+  let users = [
+    { id: 1, username: 'Jan' },
+    { id: 2, username: 'Piotr' },
+    { id: 3, username: 'Adam' },
+    { id: 4, username: 'Maria' },
+    { id: 5, username: 'Kasia'}
+  ];
+
+  const getUsers = () => {
+    $usersList.empty();
+    users.map(u => UsersBuilder.$buildUser(u).appendTo($usersList));
+  }
+
+  const addUser = (event: any) => {
+    event.preventDefault();
+
+    let $newItem = $('#username');
+    let username = $newItem.val() as string;
+
+    if (!username) {
+      return;
+    }
+
+    $newItem.val('');
+
+    users.push({
+      id: Math.max(...users.map(u => u.id), 0) + 1,
+      username: username
+    });
+
+    toastr.success('Item created', 'Success!');
+    getUsers();
+  }
+
+  const deleteUser = (event) => {
+    event.preventDefault();
+
+    const id = $(event.target.parentElement).data('id');
+
+    users = users.filter(u => u.id != id);
+
+    toastr.success('Item deleted', 'Success!');
+    getUsers();
+  }
+
+  const updateUser = (event) => {
+    event.preventDefault();
+    const $userRow = $(event.target.parentElement);
+    const id = $userRow.data('id');
+    const user = users.find(u => u.id == id);
+
+    if (!user) {
+      return;
+    }
+    
+    const useItOnSuccess = () => $userRow.find('.edit-user-button, .delete-user-button').show();
+    const username = $userRow.find('.username').val() as string;
+    user.username = username;
+
+    toastr.success('Item updated', 'Success!');
+    getUsers();
+  }
+
+  const editUser = (event) => {
+    event.preventDefault()
+
+    const $userRow = $(event.target.parentElement);
+    const $userName = $userRow.find('.username');
+
+    $userRow.find('.username').prop('disabled', false);
+    $userRow.find('.edit-user-button, .delete-user-button').hide();
+    $userRow.find('.edit-cancel-user-button, .update-user-button').show();
+
+    $userRow.data('edited-value', $userName.val());
+  }
+
+  const cancelUserEdit = (event) => {
+    event.preventDefault()
+
+    const $userRow = $(event.target.parentElement);
+    const $userName = $userRow.find('.username');
+
+    $userRow.find('.username').prop('disabled', true);
+    $userRow.find('.edit-user-button, .delete-user-button').show();
+    $userRow.find('.edit-cancel-user-button, .update-user-button').hide();
+
+    $userName.val($userRow.data('edited-value'));
+  }
+
+  $(() => {
+    getUsers();
+
+    $('#add-user-button').click((event) => addUser(event));
+    $('body').on('click', '.delete-user-button', (event) => deleteUser(event));
+    $('body').on('click', '.edit-user-button', (event) => editUser(event));
+    $('body').on('click', '.edit-cancel-user-button', (event) => cancelUserEdit(event));
+    $('body').on('click', '.update-user-button', (event) => updateUser(event));
+  })
+})();

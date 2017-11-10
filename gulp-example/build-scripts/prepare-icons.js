0303b8f827c7b1907ba114f94b720b581ac2ba39
Michal Michalczuk <michalczukm@gmail.com>
2017-11-10 01:44:09 +0100

    resize images via sharp

--- gulp-example/build-scripts/prepare-icons.js
+++ gulp-example/build-scripts/prepare-icons.js
@@ -1,10 +1,11 @@
 const fs = require('fs-extra');
 const path = require('path');
+const sharp = require('sharp');
 
 const sizes = [ 
     { x:128, y:128 },
     { x:144, y:144 },
-    { x: 192, y: 192 }
+    { x:192, y:192 }
 ];
 
 const logoPath = path.resolve(__dirname, './../assets/logo.png');
@@ -13,8 +14,10 @@ const distPath = path.resolve(__dirname, './../dist');
 fs.ensureDir(distPath)
     .then(() => 
         sizes.forEach(size =>
-            fs.copyFile(logoPath, path.resolve(distPath, `logo-${size.x}x${size.y}.png`))
+            sharp(logoPath)
+                .resize(size.x, size.y)
+                .toFile(path.resolve(distPath, `logo-${size.x}x${size.y}.png`))
         )
     )
-    .then(() => console.log('great succes!'))
-    .catch(() => console.log('ups'));
+    .then(() => console.log('great success!'))
+    .catch((error) => console.error(`ups: ${error}`));
